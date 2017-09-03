32e764a64476683b8e6065b77f763b03879074c6
Unknown <f.kratzert@gmail.com>
2017-09-03 18:01:34 +0200

    finished gr4j implementation, validated against official excel version

--- rrmpg/models/gr4j.py
+++ rrmpg/models/gr4j.py
@@ -11,6 +11,8 @@
 
 import numpy as np
 
+from numba import njit
+
 from .basemodel import BaseModel
 from ..utils.array_checks import validate_array_input, check_for_negatives
 
@@ -117,12 +119,50 @@ class GR4J(BaseModel):
             model_params[param] = getattr(self, param)
             
         if return_storage:
-            pass
+            qsim, s_store, r_store = _simulate_gr4j(prec, pe, s_init, 
+                                                    r_init, model_params)
+            return qsim, s_store, r_store
         
         else:
-            pass
+            qsim, _, _ = _simulate_gr4j(prec, pe, s_init, r_init, 
+                                        model_params)
+            return qsim
+            
+@njit
+def _s_curve1(t, x4):
+    """Calculate the s-curve of the unit-hydrograph 1.
+    
+    Args:
+        t: timestep
+        x4: model parameter x4 of the gr4j model.
         
+    """
+    if t <= 0:
+        return 0.
+    elif t < x4:
+        return (t / x4)**2.5
+    else:
+        return 1.
+
+@njit 
+def _s_curve2(t, x4): 
+    """Calculate the s-curve of the unit-hydrograph 2.
+    
+    Args:
+        t: timestep
+        x4: model parameter x4 of the gr4j model.
         
+    """
+    if t <= 0:
+        return 0.
+    elif t <= x4:
+        return 0.5 * ((t / x4) ** 2.5)
+    elif t < 2*x4:
+        return 1 - 0.5 * ((2 - t / x4) ** 2.5)
+    else:
+        return 1.
+
+@njit           
 def _simulate_gr4j(prec, pe, s_init, r_init, model_params):
     """Actual run function of the gr4j hydrological model."""
     # Unpack the model parameters
@@ -134,20 +174,40 @@ def _simulate_gr4j(prec, pe, s_init, r_init, model_params):
     # get the number of simulation timesteps
     num_timesteps = len(prec)
     
-    # initialize empty arrays for production (s) and routing (r) store
+    # initialize empty arrays for production (s), routing (r) store and qsim
     s_store = np.zeros(num_timesteps, np.float64)
     r_store = np.zeros(num_timesteps, np.float64)
+    qsim = np.zeros(num_timesteps, np.float64)
+    
     
     # set initial values
     s_store[0] = s_init
     r_store[0] = r_init
+    
+    # calculate number of unit hydrograph ordinates
+    num_uh1 = int(np.ceil(x4))
+    num_uh2 = int(np.ceil(2*x4 + 1))
+    
+    # calculate the ordinates of both unit-hydrographs (eq. 16 & 17)
+    uh1_ordinates = np.zeros(num_uh1, np.float64)
+    uh2_ordinates = np.zeros(num_uh2, np.float64)
+    
+    for j in range(1, num_uh1 + 1):
+        uh1_ordinates[j - 1] = _s_curve1(j, x4) - _s_curve1(j - 1, x4)
+        
+    for j in range(1, num_uh2 + 1):
+        uh2_ordinates[j - 1] = _s_curve2(j, x4) - _s_curve2(j - 1, x4)
+    
+    # empty arrays to store the rain distributed through the unit hydrographs
+    uh1 = np.zeros(num_uh1, np.float64)
+    uh2 = np.zeros(num_uh2, np.float64)
     
     # Start the model simulation loop
     for t in range(1, num_timesteps):
         
         # Calculate netto precipitation and evaporation
         if prec[t] >= pe[t]:
-            p_n = prec[t] - pe[t]
+            p_n = prec[t-1] - pe[t-1]
             pe_n = 0
         
             # calculate fraction of netto precipitation that fills production 
@@ -160,7 +220,7 @@ def _simulate_gr4j(prec, pe, s_init, r_init, model_params):
         
         else:
             p_n = 0
-            pe_n = pe[t] - prec[t]
+            pe_n = pe[t-1] - prec[t-1]
             
             # calculate the fraction of the evaporation that will evaporate 
             # from the production store (eq. 4)
@@ -174,7 +234,7 @@ def _simulate_gr4j(prec, pe, s_init, r_init, model_params):
         s_store[t] = s_store[t-1] - e_s + p_s
         
         # calculate percolation from actual storage level
-        perc = s_store[t] * (1 - (1 + (4 / 9 * s_store[t] / x1)**4)**(-1/4))
+        perc = s_store[t] * (1 - (1 + (4 / 9 * s_store[t] / x1)**4)**(-0.25))
         
         # final update of the production store for this timestep
         s_store[t] = s_store[t] - perc
@@ -186,7 +246,33 @@ def _simulate_gr4j(prec, pe, s_init, r_init, model_params):
         p_r_uh1 = 0.9 * p_r 
         p_r_uh2 = 0.1 * p_r
         
+        # update the state of the rain distributed through the unit hydrographs
+        for j in range(0, num_uh1 - 1):
+            uh1[j] = uh1[j + 1] + uh1_ordinates[j] * p_r_uh1
+        uh1[-1] = uh1_ordinates[-1] * p_r_uh1
         
-                     
+        for j in range(0, num_uh2 - 1):
+            uh2[j] = uh2[j + 1] + uh2_ordinates[j] * p_r_uh2
+        uh2[-1] = uh2_ordinates[-1] * p_r_uh2
+        
+        # calculate the groundwater exchange F (eq. 18)
+        gw_exchange = x2 * (r_store[t - 1] / x3) ** 3.5
+        
+        # update routing store
+        r_store[t] = max(0, r_store[t - 1] + uh1[0] + gw_exchange)
+        
+        # outflow of routing store
+        q_r = r_store[t] * (1 - (1 + (r_store[t] / x3)**4)**(-0.25))
+        
+        # subtract outflow from routing store level
+        r_store[t] = r_store[t] - q_r
+        
+        # calculate flow component of unit hydrograph 2
+        q_d = max(0, uh2[0] + gw_exchange)
+        
+        # total discharge of this timestep
+        qsim[t] = q_r + q_d
+        
+    return qsim, s_store, r_store
             
         
