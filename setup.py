b82a721abf62e246aa39c6635c0885a772d8c96b
Frederik Kratzert <f.kratzert@gmail.com>
2025-12-11 09:19:18 +0000

    Add CI-tests (#22)
    * Add CI-tests
    
    * Fix typo

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 import rrmpg
+
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
 
 setup(
     name="rrmpg",
@@ -9,13 +13,15 @@ setup(
     description=("Rainfall-Runoff-Model-PlayGround: a Python library for"
                  "hydrological modeling."),
     url="https//www.github.com/kratzert/RRMPG",
-    packages=["rrmpg", "rrmpg.models", "rrmpg.tools", "rrmpg.utils", "rrmpg.data"],
+    packages=find_packages(),
     include_package_data=True,
+    install_requires=requirements,
     license="MIT-License",
     keywords="hydrology rainfall-runoff modeling",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License"]
+        "License :: OSI Approved :: MIT License"],
+    python_requires=">=3.11",
     )
